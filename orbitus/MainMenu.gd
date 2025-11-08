5198e8fdc033722bde7b1eb3c728efadc00f827f
JKeene200 <145154917+JKeene200@users.noreply.github.com>
2025-11-07 22:10:35 -0600

    Update MainMenu.gd

--- orbitus/MainMenu.gd
+++ orbitus/MainMenu.gd
@@ -11,8 +11,5 @@ func _process(_delta: float) -> void:
 	pass
 
 # Start game when the play button is pressed.
-
-
-
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://game.tscn") # Replace with function body.
