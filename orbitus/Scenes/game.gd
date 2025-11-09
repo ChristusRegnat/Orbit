ef9437ab76b652c5b14e74779efb0efe055bf92d
MrYoYo88 <287231@student.cofo.edu>
2025-11-08 22:58:52 -0600

    Randomized Enemy Spawns

--- orbitus/Scenes/game.gd
+++ orbitus/Scenes/game.gd
@@ -1,16 +1,31 @@
 extends Node2D
 const Enemy_Ship_Scene: PackedScene = preload("res://Scenes/enemy_L.tscn")
 var counter = 0
+var rand_x = 0
+var rand_y = 0
 
 func _ready():
-	pass
+	randomize()
+	
+func enemy_spawns():
+		# Generate rand_x
+		var x_range = [[-676, -576], [576, 676]]
+		var index = randi_range(0, x_range.size() -1)
+		rand_x = randi_range(x_range[index][0],x_range[index][1])
+		# Generate rand_y
+		var y_range = [[-324, -424], [324, 424]]
+		index = randi_range(0, y_range.size() -1)
+		rand_y = randi_range(y_range[index][0],y_range[index][1])
 	
 func spawn_enemy():
 	while counter != 6:
 		var enemy = Enemy_Ship_Scene.instantiate()
-		enemy.position = Vector2(200,200)
+		enemy_spawns()
+		enemy.position = Vector2(rand_x,rand_y)
 		add_child(enemy)
 		counter += 1
+	if counter == 6:
+		counter = 0
 	
 	
 func _process(delta: float) -> void:
